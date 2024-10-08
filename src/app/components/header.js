77664870a39f06ec3ac55a52f21f9dbbaca5dd39
jayan13 <jayakumar13s@gmail.com>
2024-10-08 15:28:54 +0530

    category page updated with infinite scroll

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -27,7 +27,7 @@ return <header className="main-header">
   <div className="mangalam-logo"><Link href={`/`} alt='mangalam'><Image src="/img/mangalam-logo.svg" width={427} height={59} alt="Mangalam" /></Link></div>
 
   <div className="logo-right">
-    <Link href="#" className="icon-link"><Image src="/img/newspaper.svg" width={17} height={17} alt="E-Paper" /><span>E-Paper</span></Link>
+    <Link href="https://epaper.mangalam.com" className="icon-link"><Image src="/img/newspaper.svg" width={17} height={17} alt="E-Paper" /><span>E-Paper</span></Link>
     <District />
   </div>
 </div>
