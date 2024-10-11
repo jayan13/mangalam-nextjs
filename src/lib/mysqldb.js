a7f3fa38e0051d7d6641d0e6bc02837f32d5f8d4
brandsmaya <50939631+brandsmaya@users.noreply.github.com>
2024-10-11 12:11:50 +0530

    res css

--- src/lib/mysqldb.js
+++ src/lib/mysqldb.js
@@ -6,7 +6,8 @@ let connection;
 export async function getConnection() {
   if (!connection) {
     connection = await mysql.createConnection({
-      host: process.env.DB_HOST,    
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       user: process.env.DB_USER,        
       password: process.env.DB_PASSWORD, 
       database: process.env.DB_DATABASE,     
