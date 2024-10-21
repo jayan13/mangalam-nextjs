10ea3b88371f607aff966757078e9daf0044a31c
jayan13 <jayakumar13s@gmail.com>
2024-10-21 21:28:00 +0530

    slik module removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 
 import Script from 'next/script';
 import "./globals.css";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 import Header from "./components/header";
 import Footer from "./components/footer";
