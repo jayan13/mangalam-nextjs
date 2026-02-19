4f1a2bafcf1e7d737c85f0a42ae143988c67148d
jayan13 <jayakumar13s@gmail.com>
2026-02-19 13:59:05 +0530

    cross orgin

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  allowedDevOrigins: ['dev.mangalam.com'],
+};
 /*
 const nextConfig = {
     async headers() {
