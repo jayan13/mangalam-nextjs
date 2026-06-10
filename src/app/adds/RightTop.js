74a029770db16b530e65e83733a475e3042798e7
jayan13 <jayakumar13s@gmail.com>
2026-06-10 19:31:47 +0530

    poppi add added

--- src/app/adds/RightTop.js
+++ src/app/adds/RightTop.js
@@ -3,14 +3,15 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function RightTop() {
-    // (
-       // <div className="advertisement no-margin" >
-        //  <div className="advertisement-text">Advertisement</div>
-//<div className="ad">
-//<Link href="https://www.ajce.in" target="_blank"><Image src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/uploads/ads/Amaljyothi-300.jpg`} alt={'Amaljyothi'} width={330} height={250} loading="lazy" unoptimized={process.env.NEXT_PUBLIC_IMAGE_URL.includes('mangalam.cms')} /></Link>
-         // </div>
-//        );
-       return (<></>);
+     return (
+       <div className="advertisement no-margin" >
+          <div className="advertisement-text">Advertisement</div>
+              <div className="ad">
+              <Link href="#" target="_blank"><Image src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/uploads/ads/Poppi-1.jpg`} alt={'poppi'} width={300} height={250} loading="lazy" unoptimized={process.env.NEXT_PUBLIC_IMAGE_URL.includes('mangalam.cms')} /></Link>
+          </div>
+       </div> 
+        );
+      // return (<></>);
 }
 
 
